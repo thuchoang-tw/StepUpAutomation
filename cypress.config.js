bb56cabc2341d4bc6fa8fa5edaca321efadb99f8
thuchoang-tw <thuc.hoang@thoughtworks.com>
2025-03-11 15:58:34 +0700

    modify and add readme

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
+    reporter: "junit",
+    reporterOptions: {
+    mochaFile: "cypress/results/results-[hash].xml",
+    toConsole: true,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
